e8b23ea464f7d692443a448caf8503de72c55adc
RichardGrechko736 <162537734+richardgrechko@users.noreply.github.com>
2026-01-03 14:36:52 +0500

    Update weapons.js

--- server/lib/definitions/entityAddons/weapons.js
+++ server/lib/definitions/entityAddons/weapons.js
@@ -1,3 +1,7 @@
+const { combineStats, skillSet, makeAura } = require('../facilitators.js');
+const { base, gunCalcNames, basePolygonDamage, basePolygonHealth, dfltskl, smshskl, statnames } = require('../constants.js');
+const g = require('../gunvals.js');
 Class.dualiaWeapon_null = {
-	
+	PARENT: "genericTank",
+	LABEL: "Null",
 }
