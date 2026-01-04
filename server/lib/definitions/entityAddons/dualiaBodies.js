91f9b7fa7acfdd8e39553988f5ea4345fe145f48
RichardGrechko736 <162537734+richardgrechko@users.noreply.github.com>
2026-01-04 23:44:25 +0500

    Create dualiaBodies.js

---
+++ server/lib/definitions/entityAddons/dualiaBodies.js
@@ -0,0 +1,44 @@
+const { combineStats, skillSet, makeAura, makeAuto, makeRadialAuto } = require('../facilitators.js');
+const { base, gunCalcNames, basePolygonDamage, basePolygonHealth, dfltskl, smshskl, statnames } = require('../constants.js');
+const g = require('../gunvals.js');
+const dualia = {
+	gun(l,w,a,x,y,deg,t) {
+		return {
+			LENGTH: 20*l,
+			WIDTH: 20*w,
+			ASPECT: a,
+			X: 20*x,
+			Y: 10*y,
+			ANGLE: deg,
+			DELAY: t,
+		}
+	},
+	turret(s,x,y,an,arc,l) {
+		return {
+			SIZE: s,
+			X: 10*x,
+			Y: 5*y,
+			ANGLE: an,
+			ARC: 2*arc,
+			LAYER: l,
+		}
+	},
+	prop(s,x,y,an,l) {
+		return {
+			SIZE: s,
+			X: 10*x,
+			Y: 10*y,
+			ANGLE: an,
+			LAYER: l,
+		}
+	},
+}
+Class.dualiaBody_null = {
+	PARENT: "genericTank",
+	LABEL: "",
+	PROPS: [
+		{
+			
+		}
+	]
+}
